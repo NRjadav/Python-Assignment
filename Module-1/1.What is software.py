42121565153babbc26c8c5ad1db31e837c75f28b
NRjadav <109718224+NRjadav@users.noreply.github.com>
2022-10-09 12:14:29 -0700

    Python

---
+++ Module-1/1.What is software.py
@@ -0,0 +1,19 @@
+
+"""
+Q1:What is software ?
+
+ANS:1==> The programs and other operating information used by a computer.
+
+ANS:2==> Software is a set of computer programs and associated documentation and data.
+
+ANS:3==> This is in contrast hardware,from which the system is built and which actully performs the work.
+
+"""
+
+
+
+
+
+
+
+
